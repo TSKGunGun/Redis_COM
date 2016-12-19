68d651b8ca4d3a2e082aa2e0acb6b5a73f0bd47d
植田 卓 <stonebeach.dakar@gmail.com>
2016-12-19 20:22:14 +0900

    Add Basic Read/Write

---
+++ Redis_COM/ConnectionMultiplexer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using System.ComponentModel;
+using StackExchange.Redis;
+
+
+namespace StackExchange.Redis_COM
+{
+    [ComVisible(true)]
+    public interface IConnectionMultiplexer
+    {
+        [Description("Create a new ConnectionMultiplexer instance")]
+        ConnectionMultiplexer Connect(string configuration);
+
+        [Description("Obtain an interactive connection to a database inside redis")]
+        Database GetDatabase(int db = -1);
+    }
+
+    [ComVisible(true)]
+    [ClassInterface(ClassInterfaceType.None)]
+    public class ConnectionMultiplexer : IConnectionMultiplexer
+    {
+        private Redis.ConnectionMultiplexer ConnectionMultiplexerInstance;
+
+        public ConnectionMultiplexer Connect(string configuration)
+        {
+            ConnectionMultiplexerInstance = Redis.ConnectionMultiplexer.Connect(configuration);
+            return this;
+         }
+
+        public Database GetDatabase(int db = -1)
+        {
+            return new Database(ConnectionMultiplexerInstance.GetDatabase(db));
+        }
+    }
+}
---
+++ Redis_COM/Database.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+namespace StackExchange.Redis_COM
+{
+    [ComVisible(true)]
+    public interface IDatabase
+    {
+        void StringSet(string key, string value);
+        string StringGet(string key);
+    }
+
+    [ComVisible(true)]
+    [ClassInterface(ClassInterfaceType.None)]
+    public class Database : IDatabase
+    {
+        private Redis.IDatabase IDatabaseInstance;
+
+        public Database(Redis.IDatabase IDatabase)
+        {
+            IDatabaseInstance = IDatabase;
+        }
+
+        public void StringSet(string key, string value)
+        {
+            IDatabaseInstance.StringSet(key, value);
+        }
+
+        public string StringGet(string key)
+        {
+            return IDatabaseInstance.StringGet(key);
+        }
+    }
+}
