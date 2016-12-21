be05676229a9381e598539668ea923efa24acfc2
植田 卓 <stonebeach.dakar@gmail.com>
2016-12-21 22:20:42 +0900

    add KeyDelete method

--- Redis_COM/Database.cs
+++ Redis_COM/Database.cs
@@ -17,8 +17,14 @@ namespace StackExchange.Redis_COM
         [Description("Sets the given keys to their respective values. If 'not exists' is specified, this will not perform any operation at all even if just a single key already exists.")]
         string StringGet(string key);
 
-        [Description("")]
+        [Description("Returns if key exists.")]
         bool KeyExists(string key);
+
+        [Description("e if the key was removed.")]
+        bool KeyDelete(string key);
+
+        [Description("The number of keys that were removed.")]
+        long KeysDelete(string[] keys);
     }
 
     [ComVisible(true)]
@@ -45,6 +51,25 @@ namespace StackExchange.Redis_COM
         public bool KeyExists(string key)
         {
             return IDatabaseInstance.KeyExists(key);
+        }
+
+        public bool KeyDelete(string key)
+        {
+            return IDatabaseInstance.KeyDelete(key);
+        }
+
+        public long KeysDelete(string[] keys)
+        {
+        
+            Redis.RedisKey[] RedisKeys;
+            RedisKeys = new Redis.RedisKey[keys.ToArray().Count()];  
+            
+            for(int i=0; i > keys.ToArray().Count(); i++)
+            {
+                RedisKeys[i] = keys[i];
+            } 
+
+            return IDatabaseInstance.KeyDelete(RedisKeys);
         }
 
     }
